8dc50a5e4bfde270d09a48b79cf6b142e6a631f1
mujeebalish <mujeebrehman12122@gmail.com>
2025-02-24 19:40:52 -0800

    login page & registerBtn

--- App.js
+++ App.js
@@ -2,3 +2,10 @@ let name = document.getElementById("name");
 let email = document.getElementById("floatingInput");
 let password = document.getElementById("floatingPassword");
 let confirmPassword = document.getElementById("floatingcofirmPassword");
+let registerBtn = document.getElementById("registerBtn");
+// console.log(registerBtn);
+
+registerBtn.addEventListener("click", () => {
+
+    console.log("asjhkghjsgdghajs");
+})
--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 
-  // Import the functions you need from the SDKs you need
+  
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyDmRcaCmxogM0lxMKfayi3id_urOklKVdw",
     authDomain: "login-page-8769d.firebaseapp.com",
@@ -19,4 +14,4 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+
